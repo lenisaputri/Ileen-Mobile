17b216a69ade08598aa3e8150e97c60e0f1de8c3
lenisaputri <lenisaputri98@gmail.com>
2019-10-08 10:01:26 +0700

    Menambahkan onClick pada activity InputNama

--- app/src/main/java/com/example/ileen_mobile/login/InputNamaActivity.java
+++ app/src/main/java/com/example/ileen_mobile/login/InputNamaActivity.java
@@ -2,9 +2,13 @@ package com.example.ileen_mobile.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.ileen_mobile.MainActivity;
 import com.example.ileen_mobile.R;
+import com.example.ileen_mobile.menu.MenuActivity;
 
 public class InputNamaActivity extends AppCompatActivity {
 
@@ -12,5 +16,10 @@ public class InputNamaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_nama);
+    }
+
+    public void menuKlik(View view) {
+        Intent i = new Intent(InputNamaActivity.this, MenuActivity.class);
+        startActivity(i);
     }
 }
