8a65050d7a9002042d504efafe018c1b2a76abc7
lenisaputri <lenisaputri98@gmail.com>
2019-10-08 08:40:49 +0700

    PERTAMA

---
+++ app/src/main/java/com/example/ileen_mobile/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ileen_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.ileen_mobile.login.PlayActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+    }
+}
