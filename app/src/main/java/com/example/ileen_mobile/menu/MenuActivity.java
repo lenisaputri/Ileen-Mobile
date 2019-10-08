3b88ce21628e4fbfb10cf035504823cbbac99dfb
septakusumaningtyas <septakusumaningtyas@gmail.com>
2019-10-08 13:58:55 +0700

    Menambah icon pada menu

--- app/src/main/java/com/example/ileen_mobile/menu/MenuActivity.java
+++ app/src/main/java/com/example/ileen_mobile/menu/MenuActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 
 import com.example.ileen_mobile.R;
 import com.example.ileen_mobile.login.PlayActivity;
+import com.example.ileen_mobile.menu.fragments.MenuFragment1;
 import com.example.ileen_mobile.menu.fragments.MenuFragment2;
 
 public class MenuActivity extends AppCompatActivity {
@@ -33,8 +34,8 @@ public class MenuActivity extends AppCompatActivity {
         }
         else {
             FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_left);
-            fragmentTransaction.replace(R.id.frameLayout,new MenuFragment2(),"MENU_FRAGMENT");
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            fragmentTransaction.replace(R.id.frameLayout,new MenuFragment1(),"MENU1_FRAGMENT");
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         }
@@ -48,7 +49,7 @@ public class MenuActivity extends AppCompatActivity {
         }
         else {
             FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_left);
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
             fragmentTransaction.replace(R.id.frameLayout,new MenuFragment2(),"MENU2_FRAGMENT");
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
