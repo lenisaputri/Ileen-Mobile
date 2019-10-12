47e986d5712a7a3f4b73228ed612b4357da70b97
septakusumaningtyas <septakusumaningtyas@gmail.com>
2019-10-12 20:44:07 +0700

    Menu Practice (tampilan)

---
+++ app/src/main/java/com/example/ileen_mobile/practice/OneActivity.java
@@ -0,0 +1,16 @@
+package com.example.ileen_mobile.practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ileen_mobile.R;
+
+public class OneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
--- app/src/main/java/com/example/ileen_mobile/practice/PracticeActivity.java
+++ app/src/main/java/com/example/ileen_mobile/practice/PracticeActivity.java
@@ -2,9 +2,16 @@ package com.example.ileen_mobile.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 
 import com.example.ileen_mobile.R;
+import com.example.ileen_mobile.menu.MenuActivity;
+import com.example.ileen_mobile.weather.WeatherActivity;
 
 public class PracticeActivity extends AppCompatActivity {
 
@@ -12,5 +19,10 @@ public class PracticeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+    }
+
+    public void oneHandler(View view) {
+        Intent intent =  new Intent(this, OneActivity.class);
+        startActivity(intent);
     }
 }
