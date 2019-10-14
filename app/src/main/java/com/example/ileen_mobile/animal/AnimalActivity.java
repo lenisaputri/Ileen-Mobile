86927d94749392bc7f6b1afc2aaa972ac923c34a
septakusumaningtyas <septakusumaningtyas@gmail.com>
2019-10-14 09:28:37 +0700

    Membenarkan layout menu materi

--- app/src/main/java/com/example/ileen_mobile/animal/AnimalActivity.java
+++ app/src/main/java/com/example/ileen_mobile/animal/AnimalActivity.java
@@ -1,17 +1,47 @@
 package com.example.ileen_mobile.animal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.ileen_mobile.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class AnimalActivity extends AppCompatActivity {
+public class AnimalActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal);
+        loadFragment(new MateriFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_materi:
+                fragment = new MateriFragment();
+                break;
+            case R.id.action_gambar:
+                fragment = new GambarFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
 }
