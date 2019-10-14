214cab4393422352ac85e3dd3c444e1fa21bb21a
Hesti <hestianisa94@gmail.com>
2019-10-14 11:31:11 +0700

    clock activity

--- app/src/main/java/com/example/ileen_mobile/clock/ClockActivity.java
+++ app/src/main/java/com/example/ileen_mobile/clock/ClockActivity.java
@@ -1,16 +1,50 @@
 package com.example.ileen_mobile.clock;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.ileen_mobile.R;
+import com.example.ileen_mobile.animal.GambarFragment;
+import com.example.ileen_mobile.animal.MateriFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class ClockActivity extends AppCompatActivity {
+public class ClockActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clock);
+        loadFragment(new MateriClockFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_materi:
+                fragment = new MateriClockFragment();
+                break;
+            case R.id.action_gambar:
+                fragment = new GambarClockFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
