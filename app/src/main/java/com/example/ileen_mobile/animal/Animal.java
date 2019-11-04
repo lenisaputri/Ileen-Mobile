9a0f913fb7911d346de778985e473070c049ba75
septakusumaningtyas <septakusumaningtyas@gmail.com>
2019-11-04 09:30:54 +0700

    Menu gambar animal

--- app/src/main/java/com/example/ileen_mobile/animal/Animal.java
+++ app/src/main/java/com/example/ileen_mobile/animal/Animal.java
@@ -3,16 +3,22 @@ package com.example.ileen_mobile.animal;
 import android.graphics.drawable.Drawable;
 
 public class Animal {
-    public String judul;
+//    public String judul;
     public Drawable image;
 
-    public String getJudul() {
-        return judul;
+    public Animal(Drawable image)
+    {
+//        this.judul = judul;
+        this.image = image;
     }
 
-    public void setJudul(String judul) {
-        this.judul = judul;
-    }
+//    public String getJudul() {
+//        return judul;
+//    }
+//
+//    public void setJudul(String judul) {
+//        this.judul = judul;
+//    }
 
     public Drawable getImage() {
         return image;
